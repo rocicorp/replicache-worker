368c0db1d6e330db854a084ca7caf07b73e57ad4
Aaron Boodman <aaron@aaronboodman.com>
2021-09-06 01:46:20 -1000

    dagstore work now

--- src/counter.ts
+++ src/counter.ts
@@ -1,32 +1,46 @@
-import { StoreImpl } from "./kv";
+import { StoreImpl as KVStore } from "./kv";
+import { Store as DAGStore } from "./replicache/src/dag/store";
+import { Map as ProllyMap } from "./replicache/src/prolly/map";
+import { initHasher } from "./replicache/src/hash";
 
 export class CounterTs {
-  _store: StoreImpl;
+  _store: DAGStore;
 
   constructor(state: DurableObjectState, env: Env) {
-    this._store = new StoreImpl(state);
+    this._store = new DAGStore(new KVStore(state));
+    state.waitUntil(initHasher());
   }
 
   // Handle HTTP requests from clients.
   async fetch(request: Request) {
     return await this._store.withWrite(async (tx) => {
+      const read = tx.read();
+      let mainHash = await read.getHead("main");
+      const map = mainHash ? (await ProllyMap.load(mainHash, read)) : ProllyMap.new();
+
       // Apply requested action.
       let url = new URL(request.url);
-      let current = ((await tx.get("value")) ?? 0 ) as number;
+      let current = ((await map.get("value")) ?? 0 ) as number;
       switch (url.pathname) {
       case "/increment":
         current += 1;
-        await tx.put("value", current);
+        map.put("value", current);
         break;
       case "/decrement":
         current -= 1;
-        await tx.put("value", current);
+        map.put("value", current);
         break;
       case "/":
         break;
       default:
         return new Response("Not found", {status: 404});
       }
+
+      if (map.pendingChangedKeys().length !== 0) {
+        mainHash = await map.flush(tx);
+        await tx.setHead("main", mainHash);
+      }
+
       return new Response(current.toString());
     });
   }

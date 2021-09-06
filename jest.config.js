e9cadd5cbfaf9dc51b5623440dcb69a14753ee9c
Aaron Boodman <aaron@aaronboodman.com>
2021-09-06 01:26:34 -1000

    Don't run Replicache tests

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
   ],
   "testMatch": [
     "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+    "!**/replicache/**"
   ],
   "transform": {
     "^.+\\.(ts|tsx)$": "ts-jest"

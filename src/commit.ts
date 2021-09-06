8fcb4ba17e1d3758dc2e70399de40d2c3e2cedd3
Aaron Boodman <aaron@aaronboodman.com>
2021-09-06 04:54:32 -1000

    OK push and pull scaffolded out.

--- src/commit.ts
+++ src/commit.ts
@@ -1,10 +1,20 @@
 import { Map as ProllyMap } from "./replicache/src/prolly/map";
 import { Write } from "./replicache/src/dag/write"
-import { JSONValue } from "./replicache/src/json"
+import { deepThaw, JSONValue, ReadonlyJSONValue } from "./replicache/src/json"
 import { Chunk } from "./replicache/src/dag/chunk";
 import { Read } from "./replicache/src/dag/read";
 
-export type Commit = {
+export type LoadedCommit = {
+  data: CommitData;
+  userData: ProllyMap;
+  clientData: ProllyMap;
+}
+
+export type Client = {
+  lastMutationID: number;
+}
+
+type CommitData = {
   date: string;
   operation: Init | Mutation;  // maybe also pull if that can add data?
   userDataHash: string;
@@ -12,29 +22,64 @@ export type Commit = {
   historyHash: string;
 }
 
-export type Mutation = {
+type Mutation = {
   type: "mutation";
   clientID: string;
   name: string;
   args: JSONValue;
 }
 
-export type Init = {
+type Init = {
   type: "init";
 }
 
-export async function readCommit(read: Read, hash: string): Promise<Commit> {
+export async function getLastMutationID(commit: LoadedCommit, clientID: string) {
+  let val = commit.clientData.get(clientID);
+  if (val === null || val === undefined) {
+    val = { lastMutationID: 0 }
+    commit.clientData.put(clientID, val);
+    return 0;
+  }
+  val = val as ReadonlyJSONValue;
+  const client = deepThaw(val) as Client;
+  return client.lastMutationID;
+}
+
+export async function setLastMutationID(commit: LoadedCommit, clientID: string, lastMutationID: number) {
+  const val = (await commit.clientData.get(clientID)) as Client;
+  val.lastMutationID = lastMutationID;
+  commit.clientData.put(clientID, val);
+}
+
+export async function flushCommit(write: Write, commit: LoadedCommit): Promise<void> {
+  commit.data.userDataHash = await commit.userData.flush(write);
+  commit.data.clientsHash = await commit.clientData.flush(write);
+  const chunk = Chunk.new(commit.data, [...new Set([commit.data.userDataHash, commit.data.clientsHash, commit.data.historyHash])]);
+  await write.putChunk(chunk);
+  await write.setHead("main", chunk.hash);
+}
+
+export async function loadCommit(read: Read, hash: string): Promise<LoadedCommit> {
+  const cd = await readCommit(read, hash);
+  return {
+    data: cd,
+    clientData: await ProllyMap.load(cd.clientsHash, read),
+    userData: await ProllyMap.load(cd.userDataHash, read),
+  };
+}
+
+export async function readCommit(read: Read, hash: string): Promise<CommitData> {
   const chunk = await read.getChunk(hash);
   if (!chunk) {
     throw new Error(`chunk ${hash} not found`);
   }
-  return chunk.data as Commit;
+  return chunk.data as CommitData;
 }
 
 export async function initChain(write: Write) {
   const map = ProllyMap.new();
   const emptyMapHash = await map.flush(write);
-  const commit: Commit = {
+  const commit: CommitData = {
     date: new Date().toISOString(),
     operation: { type: "init" },
     userDataHash: emptyMapHash,
@@ -42,5 +87,10 @@ export async function initChain(write: Write) {
     historyHash: emptyMapHash,
   };
   await write.putChunk(Chunk.new(commit, [emptyMapHash]));
-  return commit;
+  const loaded: LoadedCommit = {
+    data: commit,
+    clientData: map,
+    userData: ProllyMap.new(),
+  }
+  return loaded;
 }
--- src/durable-replicache.ts
+++ src/durable-replicache.ts
@@ -2,8 +2,27 @@ import { StoreImpl as KVStore } from "./kv";
 import { Store as DAGStore } from "./replicache/src/dag/store";
 import { Map as ProllyMap } from "./replicache/src/prolly/map";
 import { initHasher } from "./replicache/src/hash";
-import { initChain, readCommit } from "./commit";
-import { Chunk } from "./replicache/src/dag/chunk";
+import { flushCommit, getLastMutationID, initChain, loadCommit, LoadedCommit, setLastMutationID } from "./commit";
+//import { MutatorDefs } from "./replicache/src/replicache";
+import { WriteTransaction } from "./replicache/src/transactions";
+import { Read } from "./replicache/src/dag/read";
+import { PullResponse } from "./replicache/src/puller";
+import { deepThaw, JSONValue } from "./replicache/src/json";
+import { PushRequest } from "./replicache/src/sync/push";
+import { Write } from "./replicache/src/dag/write";
+import { ScanResult } from "./replicache/src/scan-iterator";
+//import { ScanOptions, KeyTypeForScanOptions } from "./replicache/src/scan-options";
+
+const mutators = {
+  "increment": async (tx: WriteTransaction, delta: number) => {
+    const prev = ((await tx.get("counter")) ?? 0 ) as number;
+    if (delta === 0) {
+      return prev;
+    }
+    const next = prev + delta;
+    await tx.put("counter", next);
+  }
+};
 
 export class DurableReplicache {
   _store: DAGStore;
@@ -18,37 +37,120 @@ export class DurableReplicache {
     return await this._store.withWrite(async (tx) => {
       const read = tx.read();
       let mainHash = await read.getHead("main");
-      const commit = await (mainHash ? readCommit(read, mainHash) : initChain(tx));
-      const map = await ProllyMap.load(commit.userDataHash, read);
+      const commit = await (mainHash ? loadCommit(read, mainHash) : initChain(tx));
 
       // Apply requested action.
       let url = new URL(request.url);
-      let current = ((await map.get("value")) ?? 0 ) as number;
       switch (url.pathname) {
-      case "/increment":
-        current += 1;
-        map.put("value", current);
-        break;
-      case "/decrement":
-        current -= 1;
-        map.put("value", current);
-        break;
-      case "/":
-        break;
-      default:
-        return new Response("Not found", {status: 404});
+        case "/replicache-pull":
+          return await pull(commit, read);
+        case "/replicache-push":
+          return await push(commit, tx, request);
       }
 
-      if (map.pendingChangedKeys().length !== 0) {
-        commit.userDataHash = await map.flush(tx);
-        const chunk = Chunk.new(commit, [...new Set([commit.clientsHash, commit.historyHash, commit.userDataHash])]);
-        await tx.putChunk(chunk);
-        await tx.setHead("main", chunk.hash);
-      }
-
-      return new Response(current.toString());
+      await flushCommit(tx, commit);
+      return new Response("ok");
     });
   }
 }
 
+// TODO(aa): It would be nice to just use the WriteTransactionImpl from inside
+// Replicache, but it's difficult to do so because of all the embed goop. Once
+// that is cleaned up, can replace this with the real one.
+class WriteTransactionImpl implements WriteTransaction {
+  constructor(map: ProllyMap) {
+    this._map = map;
+  }
+
+  private _map: ProllyMap;
+
+  async put(key: string, value: JSONValue): Promise<void> {
+    this._map.put(key, value);
+  }
+  async del(key: string): Promise<boolean> {
+    const had = await this._map.has(key);
+    if (had) {
+      this._map.del(key);
+    }
+    return had;
+  }
+  async get(key: string): Promise<JSONValue | undefined> {
+    const v = await this._map.get(key);
+    if (v === undefined) {
+      return v;
+    }
+    return deepThaw(v);
+  }
+  async has(key: string): Promise<boolean> {
+    return await this._map.has(key);
+  }
+  async isEmpty(): Promise<boolean> {
+    const {done} = (this._map.entries().next()) as {done: boolean};
+    return done;
+  }
+  scan(): ScanResult<string> {
+    throw new Error("not implemented");
+  }
+  scanAll(): Promise<[string, JSONValue][]> {
+    throw new Error("not implemented");
+  }
+}
+
+async function push(commit: LoadedCommit, write: Write, request: Request): Promise<Response> {
+  const pushRequest = (await request.json()) as PushRequest; // TODO: validate
+  let lastMutationID = await getLastMutationID(commit, pushRequest.clientID);
+
+  const tx = new WriteTransactionImpl(commit.userData);
+
+  for (let mutation of pushRequest.mutations) {
+    const expectedMutationID = lastMutationID + 1;
+
+    if (mutation.id < expectedMutationID) {
+      console.log(`Mutation ${mutation.id} has already been processed - skipping`);
+      continue;
+    }
+    if (mutation.id > expectedMutationID) {
+      return new Response(`Mutation ${mutation.id} is from the future`, {status: 500});
+    }
+
+    const mutator = (mutators as any)[mutation.name];
+    if (!mutator) {
+      console.error(`Unknown mutator: ${mutation.name} - skipping`);
+    }
+
+    try {
+      await mutator(tx, mutation.args);
+    } catch (e) {
+      console.error(`Error execututation mutator: ${JSON.stringify(mutator)}: ${e.message}`);
+    }
+
+    lastMutationID = expectedMutationID;
+  }
+
+  await setLastMutationID(commit, pushRequest.clientID, lastMutationID);
+
+  return new Response("OK");
+}
+
+async function pull(commit: LoadedCommit, read: Read): Promise<Response> {
+  const pullResonse: PullResponse = {
+    cookie: null,
+    lastMutationID: 0,
+    patch: [
+      { op: "clear" as const },
+      ...[...commit.userData.entries()].map(([key, value]) => ({
+        op: "put" as const,
+        key,
+        value: deepThaw(value),
+      }))
+    ]
+  };
+  return new Response(JSON.stringify(pullResonse), {
+    headers: {
+      "Content-type": "application/javascript",
+    },
+  });
+}
+
 interface Env {}
+
